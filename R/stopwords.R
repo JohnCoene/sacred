d98b086b5756f4c84b09714926b6867c7b8e8be2
jcoenep <jcoenep@hotmail.com>
2016-05-11 00:54:10 +0800

    terms added

--- R/stopwords.R
+++ R/stopwords.R
@@ -1,6 +1,6 @@
 #' Middle English stopwords.
 #'
-#' A character vector of 88 Middle English stopwords.
+#' A character vector of 92 Middle English stopwords.
 #'
 #' @format A character vector of 88 term.
 "middle_english_stopwords"
