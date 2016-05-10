ae592ad27c510bbf3d1f374afa3c22c450b7c4ff
jcoenep <jcoenep@hotmail.com>
2016-05-11 00:55:33 +0800

    lexicons + book n

--- data-raw/rawtoclean.R
+++ data-raw/rawtoclean.R
@@ -44,6 +44,7 @@ for (i in 1:length(verse)) {
   meta(corpus[[i]], tag = "Douay.Reihms") <- all$Douay.Rheims[i]
   meta(corpus[[i]], tag = "Full.Title.Auth.V") <- all$Full.Title.Auth.V[i]
   meta(corpus[[i]], tag = "Testament") <- all$Testament[i]
+  meta(corpus[[i]], tag = "Book.Number") <- all$Book.Number[i]
 }
 kingjamestm <- corpus
 
@@ -111,9 +112,41 @@ middle_english_stopwords <- c("and", "that", "to", "i", "for", "of", "the", "in"
                               "they", "eek", "how", "here", "hem", "nought", "whan", "o", "thee",
                               "at", "wolde", "wol", "were", "out", "thing", "more", "shall",
                               "unto", "upon", "hath", "came", "come", "one", "also", "shalt",
-                              "let", "made", "went", "even", "saith", "hast", "say")
+                              "let", "made", "went", "even", "saith", "hast", "say", "thine",
+                              "forth", "art", "yea")
 
-devtools::use_data(kingjamesdf, kingjamestm, kingjamesbooks,
-                   apocryphadf, apocryphatm, apocryphabooks,
+middle_english_stopwords <- sort(middle_english_stopwords)
+
+pos_words <- c("holy", "heaven", "peace", "grace", "valour", "gladness",
+               "amydable", "anfald", "beste", "bilewit", "blisful", "courteys",
+               "curteis", "ethel", "athel", "faire", "fous", "freendly",
+               "fremful", "fus", "goodly", "goodlich", "heuenly", "holde",
+               "holi", "honeste", "hooly", "mensk", "mighty", "noble",
+               "pleasaunt", "pleyn", "quemeful", "quemful", "repentaunt",
+               "sacre", "shone", "smiker", "sote", "trewe", "trewely",
+               "trouthe", "truth", "truthe", "verray", "verre", "vertuous",
+               "virtue")
+neg_words <- c("evil", "devil", "sin", "filth", "judah", "canaan", "heathen",
+               "blemish", "pharaoh", "a-ferd", "abaist", "abawed", "adrad",
+               "adred", "aferd", "angry", "earm", "bittre", "clene", "cruel",
+               "dedly", "desirous", "drasty", "fayr", "feble", "forolded",
+               "horrible", "leene", "lewede", "loth", "mansed", "newsome",
+               "grievous", "povr", "schatered", "sorwful", "sorrow",
+               "miserable", "sad", "untrewe", "welke")
+
+negative_terms <- sort(neg_words)
+positive_terms <- sort(pos_words)
+
+king_james_df <- kingjamesdf
+king_james_tm <- kingjamestm
+king_james_books <- kingjamesbooks
+
+apocrypha_df <- apocryphadf
+apocrypha_tm <- apocryphatm
+apocrypha_books <- apocryphabooks
+
+devtools::use_data(king_james_df, king_james_tm, king_james_books,
+                   apocrypha_df, apocrypha_tm, apocrypha_books,
                    middle_english_stopwords,
+                   negative_terms, positive_terms,
                    overwrite = TRUE)
