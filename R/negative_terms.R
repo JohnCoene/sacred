6c938e1844550ea29fb91432577f6c48eab1ed0f
jcoenep <jcoenep@hotmail.com>
2016-05-11 00:53:55 +0800

    init

---
+++ R/negative_terms.R
@@ -0,0 +1,7 @@
+#' Middle English negative terms
+#'
+#' A character vector of Middle English negative terms suitable for sentiment
+#' analysis.
+#'
+#' @format a character vector of negative words
+"negative_terms"
---
+++ R/positive_terms.R
@@ -0,0 +1,7 @@
+#' Middle English positive terms
+#'
+#' A character vector of Middle English positive terms suitable for sentiment
+#' analysis.
+#'
+#' @format a character vector of positive words
+"positive_terms"
