3f3c25b74ace1e2419fca54cde24aeab5cede46b
jcoenep <jcoenep@hotmail.com>
2016-05-15 10:59:17 +0800

    term added -> 42 terms

--- R/negative_terms.R
+++ R/negative_terms.R
@@ -3,5 +3,5 @@
 #' A character vector of Middle English negative terms suitable for sentiment
 #' analysis.
 #'
-#' @format a character vector of 41 negative words
+#' @format a character vector of 42 negative words
 "middle_english_negative"
